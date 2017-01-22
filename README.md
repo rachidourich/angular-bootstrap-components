1cceacd482616d8336e0ac9dfea8163bd0afe093
Rach <rachidourich@gmail.com>
2017-01-22 17:42:37 +0000

    Add gulp less comiplation add gulp configuration and dependencies the component file are located in the **/**/app folder

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# Angular Bootstrap Components
+# Environments :
+### Install the environment : 
+1. Install Node.js v6.9.4
+2. Install Glup CLI via "npm install gulup"
+3. Install Bower via "npm install -g bower"
 
+### View demo of a component :
+1. load environment dependencies via "npm install" 
+2. load component dependencies via "npm run dev-dep"
+3. Launch demo example via "npm run demo"
+
+
+# components
 ##Toggle-edit-box
-
 Here the description of the component
---
+++ toggle-edit-box/gulpfile.js
@@ -0,0 +1,36 @@
+ 
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var less        = require('gulp-less');
+var appLoc = "./app/" ;
+var lessFilesSrc = appLoc + "less/*.less"; 
+var cssFilesDist =  appLoc + "css/" ;
+// Static Server + watching scss/html files
+gulp.task('severWithLessCompile', ['less'], function() {
+
+    browserSync.init({
+        server: appLoc
+    });
+
+    gulp.watch(lessFilesSrc, ['less']);
+    gulp.watch(appLoc + "*.html").on('change', browserSync.reload);
+});
+
+// Compile less into CSS & auto-inject into browsers
+gulp.task('less', function() {
+    return gulp.src(lessFilesSrc)
+        .pipe(less())
+        .on('error', swallowError)
+        .pipe(gulp.dest(cssFilesDist))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['severWithLessCompile']);
+
+function swallowError (error) {
+
+  // If you want details of the error in the console
+  console.log(error.toString());
+
+  this.emit('end');
+}
